4fd64d7d5742af26ffe2264913fd609835ca6cf9
Rafael Grigoryan <rafogrig2003@gmail.com>
2025-09-01 21:58:09 +0400

    js practic

--- practic.js
+++ practic.js
@@ -1,26 +1,62 @@
-function findSam(arr = [10, 2, 5, 1, 9, 12], num = 17) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === num) {
-        return [i, j];
-      }
-    }
-  }
-}
+// function findSam(arr = [10, 2, 5, 1, 9, 12], num = 17) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === num) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
 
-console.log(findSam());
+// console.log(findSam());
 
-function findSamSecond(arr = [10, 2, 5, 1, 9, 12], num = 11) {
-  const obj = {};
-  for (let i = 0; i < arr.length; i++) {
-    const res = num - arr[i];
+// function findSamSecond(arr = [10, 2, 5, 1, 9, 12], num = 11) {
+//   const obj = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     const res = num - arr[i];
 
-    if (obj[res]) {
-      return [obj[res], i];
-    }
+//     if (obj[res]) {
+//       return [obj[res], i];
+//     }
 
-    obj[arr[i]] = i;
-  }
-}
-//aam of two
-console.log(findSamSecond());
+//     obj[arr[i]] = i;
+//   }
+// }
+// //aam of two
+// console.log(findSamSecond());
+
+// for (var i = 0; i < 5; i++) {
+//   function test(num) {
+//     setTimeout(function () {
+//       console.log(num);
+//     }, 3000);
+//   }
+//   test(i);
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(
+//     function (n) {
+//       console.log("function inside", n);
+//     },
+//     4000,
+//     i
+//   );
+// }
+
+const obj = {
+  name: "Rafo",
+  age: "22",
+  printName: function () {
+    console.log(this.name, this.age);
+  },
+};
+
+obj.printName();
+
+const newObj = {
+  name: "Rafo2",
+  age: "1",
+};
+
+obj.printName.call(newObj);
